c15df026f28b4db7aaf551e27b2d99f2766143b9
Ryan Roemer <ryan@loose-bits.com>
2017-09-14 15:20:46 -0700

    Add empty manifest built file. (#2)
    Test fixture for FormidableLabs/webpack-dashboard#189

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,14 @@ module.exports = {
     path: path.join(__dirname, "dist"),
     pathinfo: true,
     filename: '[name].js'
-  }
+  },
+  plugins:[
+    // Empty (webpack boilerplate only) manifest file per
+    // https://webpack.js.org/plugins/commons-chunk-plugin/#manifest-file
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ["empty-manifest"],
+      filename: "[name].js",
+      minChunks: Infinity
+    })
+  ]
 };
